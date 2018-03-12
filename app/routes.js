7cf725c1278f60b5e0831c47d74524b8e77e4307
laurenceberry <berry.laur@gmail.com>
2018-03-12 13:27:31 +0000

    Set up lots and modules in yml

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
-var express = require('express')
-var router = express.Router()
+var fs = require('fs');
+var yaml = require('js-yaml');
+var express = require('express');
+var router = express.Router();
 
 // Route index page
 router.get('/', function (req, res) {
@@ -8,4 +10,28 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+router.get('/buyers/:page/:yml', function (req, res) {
+  var templateData = yaml.safeLoad(
+    fs.readFileSync(__dirname + '/yml/' + req.params.yml + '.yml', {encoding: 'utf-8'})
+  );
+
+  Object.keys(req.query).map(function(key) {
+    templateData[key] = req.query[key];
+  });
+
+  templateData.storedValues = req.cookies;
+
+  console.log("--- GET", req.path);
+  console.log(JSON.stringify(templateData, null, '  '));
+
+  res.render(
+    "buyers/" + req.params.page,
+    templateData
+  );
+
+});
+
+
+
 module.exports = router
+
